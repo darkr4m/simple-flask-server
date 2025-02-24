e5bbe26a5779a9384f25cdcb213992a1a6f1ad50
max <hmwright404@gmail.com>
2025-02-24 13:03:24 -0600

    student class skeleton code

--- flaskr/classes/student.py
+++ flaskr/classes/student.py
@@ -27,12 +27,64 @@ METHODS:
     
     #get_student_by_id - class method
         Input id
-        return a single Student object 
+        return a single Student object
+    
+    #add_student - class method
+        input Student object
 
 """
 
 class Student():
     students = {}
 
-    def __init__(self):
+    def __init__(self,first_name,last_name,age,grade):
+        self._id
+        self._first_name = first_name
+        self._last_name = last_name
+        self._age = age
+        self._grade = grade
+
+    # GETTERS AND SETTERS
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def first_name(self):
+        return self._first_name
+    
+    @first_name.setter
+    def first_name(self, name):
+        self._first_name = name
+    
+    @property
+    def last_name(self):
+        return self._last_name
+    
+    @last_name.setter
+    def last_name(self, name):
+        self._last_name = name
+
+    @property
+    def age(self):
+        return self._age
+    
+    @property
+    def grade(self):
+        return self._grade
+    
+    @grade.setter
+    def grade(self, grade):
+        self._grade = grade
+
+    
+    @classmethod
+    def get_all_students(cls):
+        return cls.students
+    
+    def get_student_by_id(cls, id):
+        pass
+    
+    @classmethod
+    def add_student(cls, student):
         pass
