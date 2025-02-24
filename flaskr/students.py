a0c9d89f8ddb93fcf407af53edf76c4238a0b824
max <hmwright404@gmail.com>
2025-02-24 15:29:46 -0600

    student routes

--- flaskr/students.py
+++ flaskr/students.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 
+# from .classes.student import Student
 bp = Blueprint('students',__name__)
 
 students = [
@@ -18,29 +19,36 @@ students = [
 @bp.route('/students')
 def all_students():
     # Returns an array of all student objects available.
+    # students = [student.to_dict() for student in Student.get_all_students()]
+    # students = list(map(lambda x: x.to_dict(), Student.get_all_students()))
     return jsonify(students)
 
 @bp.route('/old_students')
-def all_students():
+def old_students():
     # Returns an array of student objects where the students are older than 20 years old.
-    return jsonify(students)
+    student_list = list(filter(lambda x: x['age'] > 20, students))
+    return jsonify(student_list)
 
 @bp.route('/young_students')
-def all_students():
+def young_students():
     # Returns an array of student objects where the students are younger than 21 years old.
-    return jsonify(students)
+    student_list = list(filter(lambda x: x['age'] < 21, students))
+    return jsonify(student_list)
 
 @bp.route('/advance_students')
-def all_students():
+def adv_students():
     # Returns an array of student objects where the students are younger than 21 and have a letter grade of "A."
-    return jsonify(students)
+    student_list = list(filter(lambda x: x['age'] > 20 and x['grade'] == 'A', students))
+    return jsonify(student_list)
 
 @bp.route('/student_names')
 # Returns an array of student objects holding only the keys of 'first_name' and 'last_name' along with their corresponding values.
-def all_students():
-    return jsonify(students)
+def student_names():
+    student_list = [{'first_name': student['first_name'], 'last_name': student['last_name']} for student in students]
+    return jsonify(student_list)
 
 @bp.route('/student_ages')
 # Returns an array of student objects holding the keys 'student_name' with the value of first and last name, and 'age' with the value of that student's age.
-def all_students():
-    return jsonify(students)
+def student_ages():
+    student_list = [{'student_name': f'{student['first_name']} {student['last_name']}','age': student['age']} for student in students]
+    return jsonify(student_list)
